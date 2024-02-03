f957201962371605fb9cbc10aac2d7053f7a9398
Olvar <olvarjort@gmail.com>
2024-02-03 21:39:42 +0100

    all

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "second":  "#F7CFF1",
+        "primary": "#CD79A4"
+      }
     },
   },
   plugins: [],
